b8a755cf13aae3f27e489eaf9f21abc5fbe018b9
nsunkad <nsunkad2@illinois.edu>
2022-07-03 16:30:10 -0500

    fdhdjksfksdfl

--- 6. Counting Point Mutations/Hamm.java
+++ 6. Counting Point Mutations/Hamm.java
@@ -6,9 +6,8 @@ Given: Two DNA strings s and t of equal length (not exceeding 1 kbp).
 Return: The Hamming distance dH(s,t).
 */
 import java.util.Scanner;
-import java.io.File;
 
-class Main {
+class Hamm {
 
   public static int hammingDistance(String s, String t)
   {
